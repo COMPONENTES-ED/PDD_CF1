9a7d578f871fa054f7214247ff867c04850dbbf2
yesnapoleondynamite <yesnapoleondynamite@gmail.com>
2025-05-12 23:04:49 -0500

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+const titulo =
+  'Modelamiento y gestión de datos para modelos de inteligencia artificial'
+
+module.exports = {
+  publicPath: '',
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `@import "@/css/_variables.sass"`,
+      },
+    },
+  },
+  chainWebpack: config => {
+    // Mantener la configuración existente del título
+    config.plugin('html').tap(args => {
+      args[0].title = titulo
+      return args
+    })
+  },
+}
